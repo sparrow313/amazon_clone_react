8a473d6734db66123b471cf6f30fd935041a4a26
sparrow313 <98008251+sparrow313@users.noreply.github.com>
2022-04-09 02:26:32 +0530

    saving context codes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { StateProvider } from "./components/StateProvider";
+import reducer, { initialState } from "./components/reducer";
 
 const container = document.getElementById("root");
 const rootContainer = ReactDOM.createRoot(container);
 rootContainer.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
